5db3ec318a8abbe181bcf23555caa7819ea395af
soji <soji7ssoji24@gmail.com>
2021-04-23 13:04:34 +0900

    任意の画面位置でボタンを出現さっせる機能の追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(function() {
+
+  var scrollTop = $(window).scrollTop();
+  if(scrollTop > 460) {
+    $('#page-top').css('display', 'block');
+  }
+
+  $(window).on('scroll', function() {
+    scrollTop = $(this).scrollTop();
+    if (scrollTop > 460) {
+      $('#page-top').fadeIn();
+    } else {
+      $('#page-top').fadeOut();
+    }
+  });
+
   $('#page-top').on('click', function(event) {
     event.preventDefault();
     $('body,html').stop().animate({scrollTop: 0}, 'fast');
-  })
-})
+  });
+});
